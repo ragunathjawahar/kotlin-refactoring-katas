d2ee64f8b385a400c4659b164924f722765d6bde
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-21 18:34:17 +0530

    test: characterization test for 2 days

--- gilded-rose/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ gilded-rose/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,14 +1,21 @@
 package com.gildedrose
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
 
 class GildedRoseTest {
   @Test
-  fun foo() {
-    val items = arrayOf(Item("foo", 0, 0))
-    val app = GildedRose(items)
-    app.updateQuality()
-    assertEquals("foo", app.items[0].name)
+  fun `basic test for 2 days`() {
+    // given
+    val outStream = ByteArrayOutputStream()
+    System.setOut(PrintStream(outStream))
+
+    // when
+    main(emptyArray())
+
+    // then
+    Approvals.verify(outStream.toString())
   }
 }
